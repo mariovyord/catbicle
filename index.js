72cb89fc2f4e33cb8b725433972962fcfd6c2352
NecroBread <mariovyordanov@gmail.com>
2022-05-21 13:20:05 +0300

    Added basic structure

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('yes');
+const express = require('express');
+const hbs = require('express-handlebars');
+const { about } = require('./controllers/about');
+const { create } = require('./controllers/create');
+const { details } = require('./controllers/details');
+const { home } = require('./controllers/home');
+const { notFound } = require('./controllers/notFound');
+
+const app = express();
+
+app.engine('hbs', hbs.create({
+	extname: '.hbs'
+}).engine);
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	console.log('>>>', req.method, req.url);
+	next();
+})
+app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static('static'));
+
+app.get('/', home);
+app.get('/about', about);
+app.get('/create', create);
+app.get('/details/:id', details);
+app.all('*', notFound);
+
+app.listen(3000, () => {
+	console.log('Server listening on port 3000...')
+	console.log('URL: http://localhost:3000')
+})
